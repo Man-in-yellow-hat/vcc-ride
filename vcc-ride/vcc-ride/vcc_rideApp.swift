eafb35d47a7b94dba1a0440e3ff6a29e8b702e70
Karen Pu <karen.c.pu@vanderbilt.edu>
2023-09-24 15:32:04 -0500

    app structure

--- vcc-ride/vcc-ride/vcc_rideApp.swift
+++ vcc-ride/vcc-ride/vcc_rideApp.swift
@@ -8,27 +8,49 @@
 import SwiftUI
 import RealmSwift
 
+/// This method loads app config details from a atlasConfig.plist we generate
+/// for the template apps.
+/// When you create your own Atlas Device Sync app, use your preferred method
+/// to store and access app configuration details.
+let theAppConfig = loadAppConfig()
+
+let atlasUrl = theAppConfig.atlasUrl
+
+let app = App(id: theAppConfig.appId, configuration: AppConfiguration(baseURL: theAppConfig.baseUrl, transport: nil, localAppName: nil, localAppVersion: nil))
+
+
 
 @main
-struct vcc_rideApp: App {
+struct vcc_rideApp: SwiftUI.App {
+    @StateObject var errorHandler = ErrorHandler(app: app)
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(errorHandler)
+                .alert(Text("Error"), isPresented: .constant(errorHandler.error != nil)) {
+                    Button("OK", role: .cancel) { errorHandler.error == nil }
+                } message: {
+                    Text(errorHandler.error?.localizedDescription ?? "")
+                }
+                .onAppear {
+                    if let atlasUrl = atlasUrl {
+                        print("To view your data in Atlas, go to this link: " + atlasUrl)
+                    }
+                }
         }
     }
 }
 
-//local only realm
-class Todo: Object {
-   @Persisted(primaryKey: true) var _id: ObjectId
-   @Persisted var name: String = ""
-   @Persisted var status: String = ""
-   @Persisted var ownerId: String
+final class ErrorHandler: ObservableObject {
+    @Published var error: Swift.Error?
 
-   convenience init(name: String, ownerId: String) {
-       self.init()
-       self.name = name
-       self.ownerId = ownerId
-   }
+    init(app: RealmSwift.App) {
+        // Sync Manager listens for sync errors.
+        app.syncManager.errorHandler = { syncError, syncSession in
+            self.error = syncError
+        }
+    }
 }
+
 
