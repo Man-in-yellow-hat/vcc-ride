680b3acdf75537a67ea1dbdd2c8d3f551d979927
Karen Pu <karen.c.pu@vanderbilt.edu>
2023-09-24 16:57:23 -0500

    app

---
+++ vcc-climb_client_20230924201229/App/Item.swift
@@ -0,0 +1,9 @@
+import RealmSwift
+
+class Item: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var isComplete = false
+    @Persisted var summary: String
+    @Persisted var owner_id: String
+    @Persisted var email: String
+}
