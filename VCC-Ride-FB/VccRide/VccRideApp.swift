b6c45787121721bb7f04747263b207f5195d2325
leej64 <junwon.lee@vanderbilt.edu>
2023-12-03 01:32:29 -0500

    Automated notification

--- VCC-Ride-FB/VccRide/VccRideApp.swift
+++ VCC-Ride-FB/VccRide/VccRideApp.swift
@@ -7,23 +7,36 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
 import GoogleSignIn
-import AuthenticationServices
 
 @main
 struct FirebaseTestApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @ObservedObject private var viewModel = MainViewModel.shared
+    @StateObject var viewModel = MainViewModel() // Create an instance of MainViewModel
     @State private var isMainViewVisible = false // Add a boolean for the startup screen
 
     var body: some Scene {
         WindowGroup {
             if self.isMainViewVisible {
                 MainView()
+                    .environmentObject(viewModel)
+                
             } else {
                 StartupScreen()
                     .onAppear() {
-                        checkPreviousSignIn()
+                        if(GIDSignIn.sharedInstance.hasPreviousSignIn() && GIDSignIn.sharedInstance.currentUser == nil) {
+                            GIDSignIn.sharedInstance.restorePreviousSignIn() { user, error in
+                                if let user = Auth.auth().currentUser {
+                                    print(user.uid)
+                                    viewModel.isLoggedIn = true
+                                    viewModel.userID = user.uid
+                                    viewModel.fetchUserRole(forUserID: user.uid)
+                                } else {
+                                    print(error ?? "unknown error")
+                                }
+                            }
+                        }
                         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                              withAnimation(.easeInOut(duration: 0.5)) {
                                  self.isMainViewVisible = true
@@ -33,48 +46,100 @@ struct FirebaseTestApp: App {
             }
         }
     }
-
-    private func checkPreviousSignIn() {
-        // Check for Google Sign-In
-        if GIDSignIn.sharedInstance.hasPreviousSignIn() && GIDSignIn.sharedInstance.currentUser == nil {
-            GIDSignIn.sharedInstance.restorePreviousSignIn() { user, error in
-                handleSignIn(user: Auth.auth().currentUser, error: error)
-            }
-        }
-        // Check for Apple Sign-In
-        else if let appleID = UserDefaults.standard.string(forKey: "appleAuthorizedUserIdKey"), Auth.auth().currentUser == nil {
-            let appleIDProvider = OAuthProvider(providerID: "apple.com")
-            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: appleID, rawNonce: nil)
-            Auth.auth().signIn(with: credential) { authResult, error in
-                handleSignIn(user: authResult?.user, error: error)
-            }
-        }
-    }
-
-    private func handleSignIn(user: User?, error: Error?) {
-        if let user = user {
-            print(user.uid)
-            viewModel.isLoggedIn = true
-            viewModel.userID = user.uid
-            viewModel.fetchUserRole(forUserID: user.uid)
-            viewModel.fetchUserNames(forUserID: user.uid) {
-                print("finished getting name")
-            }
-        } else {
-            print(error ?? "Unknown error during sign-in")
-        }
-    }
 }
-
 
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    var window: UIWindow?
+    let gcmMessageIDKey = "gcm.message_id"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
         return true
     }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+    -> UIBackgroundFetchResult {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        
+        return UIBackgroundFetchResult.newData
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNoficationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
+    -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        print(userInfo)
+        
+        return [[.alert, .sound]]
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        print(userInfo)
+    }
+}
+    
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("fcmToken: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
+}
+
 
 @available(iOS 9.0, *)
 func application(_ app: UIApplication,
